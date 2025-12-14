f46ac247a34a749c7538fe0902186b3230b24167
pratim994 <aryanpratim86@gmail.com>
2025-12-14 12:31:09 +0530

    green

--- udp_parser.py
+++ udp_parser.py
@@ -22,7 +22,7 @@ def get_peers_from_udp_tracker(announce_url, info_hash, peer_id, left, port=6881
     sock.close()
     return peers
 
-    def _udp_connect(sock, addr):
+ def _udp_connect(sock, addr):
         protocol_id = 0x41727101980
         action = 0
         transaction_id = random.randint(0, 0xFFFFFFFF)
@@ -39,4 +39,47 @@ def get_peers_from_udp_tracker(announce_url, info_hash, peer_id, left, port=6881
                     return conn_id
              except socket.timeout:
                 pass
-    return None
+ return None
+
+ def  _udp_announce(sock, addr, connection_id, info_hash, peer_id, downloaded, left, uploaded, port, numwant):
+    action =1
+    transaction_id = random.randint(0, 0xFFFFFFFF)
+    key = random.randint(0, 0xFFFFFFFF)
+    ip = 0  # default
+
+    request = struct.pack("!qii20s20sQQQiiiIH",
+                          connection_id, action, transaction_id,
+                          info_hash, peer_id,
+                          downloaded, left, uploaded,
+                          2,  # event: started
+                          ip, key, numwant, port)
+    for _ in range(5):
+            try:
+                sock.sendto(request, addr)
+                sock.settimeout(8)
+                response = sock.recv(4098)
+
+                if len(response) < 20:
+                    continue
+                action_resp. trans_id_resp = struct.unpack("!ii", response[:8])
+                if trans_id_resp != transaction_id or action_resp != 1:
+                    continue 
+                interval  = struct.unpack("!i", response[8:12])[0]
+                leachers = struct.unpack("!i", response[12:16])[0]
+                seeders =  struct.unpack("!i", response[16:20])[0]
+
+                peers = []
+                peer_data = response[20:]
+                for i in range(0, len(peer_data),6):
+                    if i+6 > len(peer_data):
+                        break
+                    ip_bytes = peer_data[i:i+4]
+                    port = struct.unpack("!H", peer_data[i+4:i:6])[0]
+                    ip = ".".join(str(b) for b in ip_bytes)
+                    peers.append((ip,port))
+                print(f"udp tracker: {len(peers)} peers (seeders ={seeder}, leechers = {leechers})")
+                return peers
+
+            except socket.timeout:
+                pass
+return []
